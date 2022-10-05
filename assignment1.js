3a601af565cb5d4bd07c50f456fbf25c28b51d01
latikaBagal <113565376+latikaBagal@users.noreply.github.com>
2022-10-05 19:50:53 +0530

    First program execution in JS
    First program execution in JS

---
+++ assignment1.js
@@ -0,0 +1,6 @@
+console.log("My name is Latika Bagal");
+console.log("My Hobbies: Reading books,Watching TV");
+console.info("My Strengths is: Honest,Hard Working,positive attitude,Problem Solver");
+console.warn("My Weakness: Communication Skill,emotional");
+console.log(" 30+50");
+console.log("49*479");
---
+++ text.js
@@ -0,0 +1,10 @@
+// This is my first program
+// Now i know the two ways to run program
+console.log("hello");
+console.log("My name is Latika");
+// info means information
+console.info("This is info");
+// warn means warning
+ console.warn("I am not that bad girl"); 
+ console.error("Opps failed to execute");
+
