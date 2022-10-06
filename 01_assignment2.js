837c9fed50618ed98f37af0238af9cbee9b0eaca
latikaBagal <113565376+latikaBagal@users.noreply.github.com>
2022-10-07 03:32:12 +0530

    java Assignment

---
+++ 01_assignment2.js
@@ -0,0 +1,28 @@
+// swap two variable using Third variable
+var sweety
+sweety="Sweety";
+var cutie;
+cutie="Cutie"
+console.log("Before Swapping");
+console.log(sweety,cutie);
+sweety="Cutie";
+temp="Sweety";
+console.log("After Swapping");
+console.log(sweety,temp);
+// swap three variable values
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("Before Swapping value", num1,num2,num3);
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("After Swapping value",num1,num2,num3);
+
+
+
+
+
+
+
--- 01_swap_variables.js
+++ 01_swap_variables.js
@@ -5,6 +5,6 @@ console.log(my_number, your_number);
 
 var temp = my_number;
 my_number=your_number;
-your_number=my_number
+your_number=temp;
 console.log("After swap");
 console.log(my_number, your_number);
