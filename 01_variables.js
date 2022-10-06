b498df4ba3724ac2eeda86e3a61ec9c84a177b6d
latikaBagal <113565376+latikaBagal@users.noreply.github.com>
2022-10-06 08:02:31 +0530

    varriable and swap variable learning
    varriable and swap variable learning

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+var my_apple;//variable declaration
+my_apple = "Apple";// variable initialization
+
+//var my_name = "Latika Bagal";//Variable declaration and initialization in tha same line
+//my_name = "Latika Gorakh Bagal";
+//console.log(my_name);
+console.log(my_apple);
+
+my_apple = "200";
+console.log(my_apple);
